b5994ffb9469ec61a1e0323f3ee25f4852a837b2
mark <bism4rk228@gmail.com>
2022-02-10 20:36:36 +0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+
+// Контакт с пользователем
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваше фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// console.log(answers);
+
+
+
+// Интерполяция
+
+// const user = "Mark";
+// alert(`Привет, ${user}`);
+
+
+// Операторы 
+
+// let incr = 10,
+//     decr = 10;
+
+// ++incr; // Инкремент ++incr (Префиксной) incr++ (Постфиксной)
+// --decr; // Декремент --decr (Префиксной) decr-- (Постфиксной)
+
+// console.log(incr++);
+// console.log(decr--);
+
+
+const isChecked = true,
+    isClose = true;
+
+console.log(isChecked && isClose);
